27fb5d638da05df73b2413fd9080ee20ea67fc4c
YasinKose <yasin.kose.123@gmail.com>
2023-07-13 01:21:00 +0300

    feat: create useEffect method in resetPassword.js

--- src/pages/resetPassword.js
+++ src/pages/resetPassword.js
@@ -1,15 +1,34 @@
 import styles from '../styles/form.module.scss';
 import {Button, Form, Input, message} from 'antd';
-import {resetPasswordHandlerTwo} from '../components/forms/FormManager';
+import {checkResetPasswordToken, resetPasswordHandlerTwo} from '../components/forms/FormManager';
 import {useClientError} from '../hooks/useClientError';
 import logo from '../assets/images/logo.jpeg';
 import {useNavigate, useParams} from 'react-router-dom';
+import {useEffect, useState} from "react";
+import Loading from "../components/Loading";
 
 const ResetPassword = () => {
   const [resetPasswordFormWithToken] = Form.useForm();
+  const [loading, setLoading] = useState(false);
   const clientError = useClientError();
   const {id} = useParams();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    setLoading(true);
+    checkResetPasswordToken(id).then(response => {
+      if (response.status === 200) {
+        message.success(response.data.message);
+        setLoading(false);
+      }
+    }).catch(err => {
+      setLoading(false);
+      message.error(err.response.data.message);
+      setTimeout(() => {
+        navigate('/');
+      }, 10)
+    })
+  }, []);
 
   const resetPasswordFormHandlerWithToken = (formValues) => {
     if (formValues.password !== formValues.password_confirmation) {
@@ -28,28 +47,31 @@ const ResetPassword = () => {
     })
   }
 
+
   return (
-    <div className={styles['formWrapper']}>
-      <Form
-        form={resetPasswordFormWithToken}
-        className={styles['form']}
-        onFinish={resetPasswordFormHandlerWithToken}
-      >
-        <img
-          src={logo}
-          alt='Company Logo'
-        />
-        <Form.Item rules={[{required: true, message: 'Bu alan zorunludur.'}]} name='password'>
-          <Input autoComplete='new-password' type='password' placeholder='Şifre'/>
-        </Form.Item>
-        <Form.Item rules={[{required: true, message: 'Bu alan zorunludur.'}]} name='password_confirmation'>
-          <Input autoComplete='new-password' type='password' placeholder='Şifre Tekrar'/>
-        </Form.Item>
-        <Button type='primary' htmlType='submit'>
-          Şifremi Sıfırla
-        </Button>
-      </Form>
-    </div>
+      loading ?
+          <Loading/> :
+          <div className={styles['formWrapper']}>
+            <Form
+                form={resetPasswordFormWithToken}
+                className={styles['form']}
+                onFinish={resetPasswordFormHandlerWithToken}
+            >
+              <img
+                  src={logo}
+                  alt='Company Logo'
+              />
+              <Form.Item rules={[{required: true, message: 'Bu alan zorunludur.'}]} name='password'>
+                <Input autoComplete='new-password' type='password' placeholder='Şifre'/>
+              </Form.Item>
+              <Form.Item rules={[{required: true, message: 'Bu alan zorunludur.'}]} name='password_confirmation'>
+                <Input autoComplete='new-password' type='password' placeholder='Şifre Tekrar'/>
+              </Form.Item>
+              <Button type='primary' htmlType='submit'>
+                Şifremi Sıfırla
+              </Button>
+            </Form>
+          </div>
   )
 }
 
