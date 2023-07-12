0e9ea491c0ed4df6ff611a9bc3269e7f913673b9
YasinKose <yasin.kose.123@gmail.com>
2023-07-13 00:50:41 +0300

    refactor: update checkAuthorization screen

--- src/pages/checkAuthorization.js
+++ src/pages/checkAuthorization.js
@@ -3,38 +3,35 @@ import {checkAuth} from '../components/forms/FormManager';
 import {useClientError} from '../hooks/useClientError';
 import {useNavigate, useParams} from 'react-router-dom';
 import styles from '../styles/form.module.scss';
-import {Button, Form, Input} from 'antd';
+import {Alert, Form, message} from 'antd';
 import logo from '../assets/images/logo.jpeg';
 import Loading from '../components/Loading';
 
 const CheckAuthorization = () => {
   const [isConfirmed, setIsConfirmed] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('Hesabınız henüz onaylanmamıştır.');
+  const [errorMessage, setErrorMessage] = useState('Hesabınız onaylanamamıştır.');
 
   const navigate = useNavigate();
   const clientError = useClientError();
-  const {token, username} = useParams();
+  const {userUuid, verificationToken} = useParams();
 
   useEffect(() => {
-    console.log(token);
     setLoading(true);
-    checkAuth(`${token}/${username}`).then(response => {
-      if(response.status === 200) {
-        message.success('Hesabınız onaylanmıştır, ana sayfaya yönlendiriliyorsunuz.');
+    checkAuth(`${userUuid}/${verificationToken}`).then(response => {
+      if (response.status === 200) {
+        message.success(response.data.message);
         setLoading(false);
         setIsConfirmed(true);
         setTimeout(() => {
           navigate('/');
-        }, 250)
-      }
-      else {
+        }, 1500)
+      } else {
         setLoading(false);
         setIsConfirmed(false);
-        message.success('Hesabınız henüz onaylanmamıştır.');
+        message.error('Hesabınız onaylanamamıştır.');
       }
     }).catch(err => {
-      clientError(err);
       setErrorMessage(err.response.data.message);
       setLoading(false);
     })
@@ -42,13 +39,18 @@ const CheckAuthorization = () => {
 
   return <div className={styles['formWrapper']}>
     <Form
-      className={styles['form']}
+        className={styles['form']}
     >
       <img
-        src={logo}
-        alt='Company Logo'
+          src={logo}
+          alt='Company Logo'
       />
-      {loading ? <Loading/> : isConfirmed ? 'Hesabınız onaylanmıştır, yönlendiriliyorsunuz.' : errorMessage}
+      {loading ?
+          <Loading/> :
+          isConfirmed ?
+              <Alert message="Hesabınız onaylanmıştır, yönlendiriliyorsunuz." type="success" showIcon/> :
+              <Alert message={errorMessage} type="error" showIcon/>
+      }
     </Form>
   </div>
 }
